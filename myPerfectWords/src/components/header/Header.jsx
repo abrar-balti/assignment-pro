bad717f6301cacdce5287a150340a558a5660451
Abrar Balti <abrarbulti@gmail.com>
2023-11-08 16:17:35 +0500

    mobile menu has been added

--- myPerfectWords/src/components/header/Header.jsx
+++ myPerfectWords/src/components/header/Header.jsx
@@ -1,18 +1,19 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faBars } from "@fortawesome/free-solid-svg-icons";
-import ResponsiveMenu from '../responsiveHeader/ResponsiveHeader';
-const Header = () => {
-   const [isMenuOpen, setIsMenuOpen] = useState(false);
+import ResponsiveMenu from "../responsiveHeader/ResponsiveHeader";
 
-   const toggleMenu = () => {
-     setIsMenuOpen(!isMenuOpen);
-   };
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
     <>
       {/* header main container */}
-      <div className="main-wrapper w-full  ">
+      <div className="main-wrapper w-full  shadow">
         {/* header-wrapper */}
         <div className="header-container flex justify-between xl:justify-evenly items-center h-30 py-3 px-2">
           {/* logo div starts */}
@@ -42,25 +43,74 @@ const Header = () => {
                 >
                   Services
                 </a>
-                <ul className="dropdown-menu hidden absolute top-full left-0 bg-white border border-gray-300 rounded-md p-2 mt-2 space-y-2">
-                  <li>
-                    <a href="#">Service 1</a>
+                <ul className="dropdown-menu hidden absolute w-[200px] p-2 border border-blue-300 rounded-md  text-sm bg-white shadow">
+                  <li className="">
+                    <a href="#">Argumentive Essay Writing</a>
                   </li>
                   <li>
-                    <a href="#">Service 2</a>
+                    <a href="#">Argumentive Essay Writing</a>
                   </li>
                   <li>
-                    <a href="#">Service 3</a>
+                    <a href="#">Argumentive Essay Writing</a>
+                  </li>
+                  <li>
+                    <a href="#">Argumentive Essay Writing</a>
+                  </li>
+                  <li>
+                    <a href="#">Argumentive Essay Writing</a>
+                  </li>
+                  <li>
+                    <a href="#">Argumentive Essay Writing</a>
                   </li>
                 </ul>
               </li>
-              <li className="px-4">
-                <a href="#" id="menu-item-dropdown">
+              <li className="px-4  group">
+                <a
+                  href="#"
+                  id="menu-item-dropdown"
+                  className="group-hover:text-blue-600"
+                >
                   Best Offers
                 </a>
+                <ul className="dropdown-menu hidden absolute w-[150px] p-2 border border-blue-300 rounded-md  text-sm bg-white shadow ">
+                  <li>
+                    <a href="#">Buy Research Paper</a>
+                  </li>
+                  <li>
+                    <a href="#">Buy Research Paper</a>
+                  </li>
+                  <li>
+                    <a href="#">Buy Research Paper</a>
+                  </li>
+                  <li>
+                    <a href="#">Buy Research Paper</a>
+                  </li>
+                  <li>
+                    <a href="#">Buy Research Paper</a>
+                  </li>
+                  <li>
+                    <a href="#">Buy Research Paper</a>
+                  </li>
+                  <li>
+                    <a href="#">Buy Research Paper</a>
+                  </li>
+                </ul>
               </li>
-              <li className="px-4">
-                <a href="#">Compnay</a>
+              <li className="px-4 relative group">
+                <a href="#" id="menu-item-dropdown">
+                  Compnay
+                </a>
+                <ul className="dropdown-menu hidden absolute w-[100px] p-2 border border-blue-300 rounded-md  text-sm bg-white shadow">
+                  <li>
+                    <a href="">About Us</a>
+                  </li>
+                  <li>
+                    <a href="">Why Us</a>
+                  </li>
+                  <li>
+                    <a href="">Other</a>
+                  </li>
+                </ul>
               </li>
               <li className="px-4">
                 <a href="#">Blog</a>
@@ -70,15 +120,29 @@ const Header = () => {
           {/* menu item div ends here */}
 
           {/* order div starts here */}
-          <div className=" hidden order-signUp xl:block  ">
-            <button className="border-2 px-3 py-1 border-slate-900">
+          <div className=" hidden order-signUp  xl:flex items-center ">
+            <button className="border px-3 py-1 border-slate-900 hover:bg-[#2D96EE] hover:text-white hover:rounded-sm hover:border-[#2D96EE]">
               AI Essay Writers
             </button>
             <button className=" mx-2 px-3 py-1 bg-[#2D96EE] text-white rounded hover:bg-[#1150CA] hover:cursor-pointer">
               Order Now
             </button>
-            <FontAwesomeIcon icon={faUser} className="user-icon" />
+            <div className="user-icon relative group">
+              <FontAwesomeIcon
+                icon={faUser}
+                className="user-icon menu-item-dropdown hover:cursor-pointer"
+              />
+              <ul className="dropdown-menu hidden absolute right-0 w-[80px] p-2 border border-blue-300 rounded-md text-sm my-4 menu-items font-semibold text-gray-600 bg-white shadow">
+                <li>
+                  <a href="#">Login</a>
+                </li>
+                <li>
+                  <a href="#">Sign Up</a>
+                </li>
+              </ul>
+            </div>
           </div>
+          {/* order div closes here */}
         </div>
       </div>
     </>
--- myPerfectWords/src/components/responsiveHeader/ResponsiveHeader.jsx
+++ myPerfectWords/src/components/responsiveHeader/ResponsiveHeader.jsx
@@ -1,34 +1,105 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUser, faBars } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faBars,
+  faEnvelope,
+  faPhone,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
+import "./ResponsiveHeader.css";
+
 const ResponsiveMenu = ({ isMenuOpen, toggleMenu }) => {
   return (
-    <div className="xl:hidden">
-      <button onClick={toggleMenu} className="text-gray-600">
-        <FontAwesomeIcon icon={faBars} size="2x" />
-      </button>
+    <div className="xl:hidden relative">
+      <div className="humberger ">
+        <button onClick={toggleMenu} className="text-gray-600">
+          <FontAwesomeIcon icon={isMenuOpen ? faTimes : faBars} size="2x" />
+        </button>
+      </div>
 
       {isMenuOpen && (
-        <div className="bg-white mt-3 p-4 border border-gray-300 rounded-md">
-          <ul>
-            <li>
-              <a href="#">Our Writers</a>
-            </li>
-            <li>
-              <a href="#">Pricing</a>
-            </li>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <a href="#">Best Offers</a>
-            </li>
-            <li>
-              <a href="#">Company</a>
-            </li>
-            <li>
-              <a href="#">Blog</a>
-            </li>
-          </ul>
+        <div className="flex flex-col flex-wrap px-2 min-h-screen bg-white border border-blue-300 rounded-sm">
+          <div className="mobile-logo">
+            <img
+              src="https://myperfectwords.com/new-land/images/mpw-service-logo.svg"
+              alt=""
+              className="w-[150px] my-3"
+            />
+          </div>
+          <div className="mobile-menu my-3  ">
+            <ul>
+              <li>
+                <a href="#">
+                  <img
+                    src="https://myperfectwords.com/images/ai-essay-writer-icon.svg"
+                    alt=""
+                  />
+                  Our Writers
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  {" "}
+                  <img
+                    src="https://myperfectwords.com/images/pricing-icon.svg"
+                    alt=""
+                  />
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  {" "}
+                  <img
+                    src="https://myperfectwords.com/images/nav-pen.svg"
+                    alt=""
+                  />
+                  Services
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img
+                    src="https://myperfectwords.com/images/company.svg"
+                    alt=""
+                  />
+                  Best Offers
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img
+                    src="https://myperfectwords.com/images/company.svg"
+                    alt=""
+                  />
+                  Company
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <img
+                    src="https://myperfectwords.com/images/blogIcon.svg"
+                    alt=""
+                  />
+                  Blog
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="contact-section  flex flex-col flex-wrap my-3">
+            <button className="bg-blue-600 text-white font-bold rounded-3xl py-3 px-4 my-5">
+              {" "}
+              Sign Up Now
+            </button>
+            <span className="flex items-center my-2">
+              <FontAwesomeIcon icon={faEnvelope} size="1x" />
+              <p className="px-2">info@assignmentpro.com</p>
+            </span>
+            <span className="flex items-center  my-2">
+              <FontAwesomeIcon icon={faPhone} size="1x" />
+              <p className="px-2">+1-345-4432-543</p>
+            </span>
+          </div>
         </div>
       )}
     </div>
